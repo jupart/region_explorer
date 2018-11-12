f1581f48416b9a7ada2c72c818c091cc27faa9d5
jupart <justinwpartain1@gmail.com>
2018-11-11 21:10:51 -0500

    Basic RegionWindow layout

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ extern crate imgui_gfx_renderer;
 extern crate imgui_glutin_support;
 extern crate image;
 
+use std::collections::HashMap;
 use imgui::*;
 use imgui_gfx_renderer::{Renderer, Shaders};
 use std::time::Instant;
@@ -14,18 +15,72 @@ use gfx::{Device};
 use glutin::{GlContext};
 
 const CLEAR_COLOR: [f32; 4] = [0.2, 0.2, 0.2, 1.0];
+const DESCRIPTION_CAPACITY: usize = 5000;
+
+struct RegionWindow {
+    name: String,
+    zoom: f32,
+    image: ImTexture,
+    image_size: (f32, f32),
+    description: Option<String>,
+    last_mouse_pos: Option<(f32, f32)>,
+    update_description: bool,
+}
+
+impl RegionWindow {
+    pub fn new(name: String, image: ImTexture, image_size: (u32, u32)) -> Self {
+        Self {
+            name,
+            zoom: 1.0,
+            image,
+            image_size: (image_size.0 as f32, image_size.1 as f32),
+            description: None,
+            last_mouse_pos: None,
+            update_description: false,
+        }
+    }
+
+    /// If returns false, this window is done, remove it from window list
+    pub fn do_ui(&mut self, ui: &Ui) -> bool {
+        let mut desc = ImString::with_capacity(DESCRIPTION_CAPACITY);
+        let desciption_string = match &self.description {
+            Some(string) => string,
+            None => "",
+        };
+        desc.push_str(desciption_string);
+
+        ui.window(im_str!("Kellua Saari"))
+            .scroll_bar(false)
+            .resizable(false)
+            .scrollable(false)
+            .size((800.0, 600.0), ImGuiCond::Once)
+            .build(|| {
+                ui.child_frame(im_str!("Map"), (500.0, 560.0))
+                    .always_show_vertical_scroll_bar(true)
+                    .scrollbar_horizontal(true)
+                    .build(|| {
+                        ui.image(self.image, ImVec2::new(self.image_size.0 * 1.0, self.image_size.1 * 1.0)).build();
+                    });
+                ui.same_line(520.0);
+                ui.child_frame(im_str!("Description"), (270.0, 580.0))
+                    .build(|| {
+                        let changed = ui.input_text_multiline(im_str!("Input"), &mut desc, ImVec2::new(270.0, 560.0)).build();
+                        if changed {
+                        }
+                    });
+            });
+        true
+    }
+}
 
 fn main() {
-    type ColorFormat = gfx::format::Rgba8;
-    type DepthFormat = gfx::format::DepthStencil;
-
     let mut events_loop = glutin::EventsLoop::new();
     let context = glutin::ContextBuilder::new().with_vsync(true);
     let window = glutin::WindowBuilder::new()
         .with_title("Region Explorer")
         .with_dimensions(glutin::dpi::LogicalSize::new(1024f64, 768f64));
     let (window, mut device, mut factory, mut main_color, mut main_depth) =
-        gfx_window_glutin::init::<ColorFormat, DepthFormat>(window, context, &events_loop);
+        gfx_window_glutin::init::<gfx::format::Rgba8, gfx::format::DepthStencil>(window, context, &events_loop);
     let mut encoder: gfx::Encoder<_, _> = factory.create_command_buffer().into();
     let shaders = {
         let version = device.get_info().shading_language;
@@ -65,11 +120,7 @@ fn main() {
         }
     }
     imgui.set_ini_filename(None);
-
-    // In the examples we only use integer DPI factors, because the UI can get very blurry
-    // otherwise. This might or might not be what you want in a real application.
     let hidpi_factor = window.get_hidpi_factor().round();
-
     let font_size = (13.0 * hidpi_factor) as f32;
 
     imgui.fonts().add_default_font_with_config(
@@ -86,6 +137,11 @@ fn main() {
 
     let (tex, sampler, size) = load_texture(&mut factory, &include_bytes!("../resources/kellua saari.png")[..]).unwrap();
     let image = renderer.textures().insert((tex, sampler));
+
+    let mut sizes = HashMap::new();
+    sizes.insert("kellua", size);
+
+    let mut region_window = RegionWindow::new("Kellua".to_owned(), image, size);
 
     imgui_glutin_support::configure_keys(&mut imgui);
 
@@ -130,16 +186,11 @@ fn main() {
 
         let frame_size = imgui_glutin_support::get_frame_size(&window, hidpi_factor).unwrap();
 
+        let mut string = ImString::with_capacity(DESCRIPTION_CAPACITY);
+        string.push_str("Test poop");
         let ui = imgui.frame(frame_size, delta_s);
-        ui.window(im_str!("Kellua Saari"))
-            .scrollable(false)
-            .horizontal_scrollbar(true)
-            .build(|| {
-                ui.image(image, ImVec2::new(size.0 as f32, size.1 as f32)).build();
-                let mouse_pos = ui.imgui().mouse_pos();
-                ui.text(im_str!("Mouse Position: ({:.1},{:.1})", mouse_pos.0, mouse_pos.1));
-            });
-
+        region_window.do_ui(&ui);
+        ui.show_demo_window(&mut true);
 
         encoder.clear(&main_color, CLEAR_COLOR);
         renderer.render(ui, &mut factory, &mut encoder).expect("Rendering failed");
