e10722f8ad2de9b1a9e3d31170f6741703d2bd6f
jupart <justinwpartain1@gmail.com>
2018-11-29 10:57:26 -0500

    Added readonly mode and saving to file

--- src/main.rs
+++ src/main.rs
@@ -26,9 +26,8 @@ const IMAGE_FRAME_WIDTH: f32 = 500.0;
 const IMAGE_FRAME_HEIGHT: f32 = 540.0;
 const TEXT_FRAME_WIDTH: f32 = 270.0;
 const TEXT_FRAME_HEIGHT: f32 = 540.0;
-const BUTTON_OFFSET: f32 = 2.0;
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone)]
 struct MapPoint {
     x: f32,
     y: f32,
@@ -51,13 +50,16 @@ struct RegionData {
 
 struct RegionWindow {
     name: String,
-    description: String,
+    region_description: String,
+    current_description: String,
     image: ImTexture,
+    image_name: String,
     image_size: (f32, f32),
     image_pos: (f32, f32),
     points: Vec<MapPoint>,
     selected_point: i32,
     zoom: f32,
+    readonly: bool,
 }
 
 impl RegionWindow {
@@ -72,6 +74,27 @@ impl RegionWindow {
     fn mouse_in_frame_coords(&self, ui: &Ui) -> (f32, f32) {
         let current_mouse_pos = ui.imgui().mouse_pos();
         (current_mouse_pos.0 - 17.0, current_mouse_pos.1 - 54.0)
+    }
+
+    fn create_backup(&self) {
+        println!("Created backup");
+        std::fs::copy("./resources/kellua_saari.ron", "./resources/kellua_saari.backup").unwrap();
+    }
+
+    fn write_file(&self) {
+        self.create_backup();
+        println!("Write dat file bebe");
+
+        let region = RegionData {
+            name: self.name.clone(),
+            image: self.image_name.clone(),
+            description: self.region_description.clone(),
+            points: self.points.clone(),
+        };
+
+        let pretty = ron::ser::PrettyConfig::default();
+        let ron_string = ron::ser::to_string_pretty(&region, pretty).unwrap();
+        std::fs::write("./resources/kellua_saari.ron", ron_string).unwrap();
     }
 
     pub fn do_ui(&mut self, ui: &Ui) {
@@ -98,7 +121,7 @@ impl RegionWindow {
         //     }
         // }
         if ui.imgui().is_mouse_clicked(ImMouseButton::Right) {
-            if self.mouse_in_region(&ui) {
+            if self.mouse_in_region(&ui) && !self.readonly {
                 let frame_click = self.mouse_in_frame_coords(&ui);
                 let point_x = frame_click.0 - self.image_pos.0;
                 let scaled_point_x = point_x / self.zoom;
@@ -114,7 +137,7 @@ impl RegionWindow {
         }
 
         let mut desc = ImString::with_capacity(DESCRIPTION_CAPACITY);
-        desc.push_str(self.description.as_str());
+        desc.push_str(self.current_description.as_str());
 
         ui.window(im_str!("Kellua Saari"))
             .position((0.0, 0.0), ImGuiCond::Once)
@@ -126,13 +149,14 @@ impl RegionWindow {
                 // Headers
                 ui.text(self.name.as_str());
                 ui.same_line(IMAGE_FRAME_WIDTH + 20.0);
-                ui.text("Description");
-                ui.same_line(IMAGE_FRAME_WIDTH + 250.0);
 
-                // Save description
-                let save = ui.small_button(im_str!("Save"));
-                if save {
-                    println!("Save it baby");
+                // Is readonly?
+                ui.checkbox(im_str!("Read-only"), &mut self.readonly);
+                ui.same_line(IMAGE_FRAME_WIDTH + 244.0);
+
+                // Write changed descriptions and new points to file
+                if ui.button(im_str!("Write"), ImVec2::new(45.0, 18.0)) && !self.readonly {
+                    self.write_file();
                 }
 
                 // Map
@@ -148,13 +172,13 @@ impl RegionWindow {
                         let mut i = 0;
                         for point in &self.points {
                             let draw_point = (
-                                self.image_pos.0 + (point.x - BUTTON_OFFSET) * self.zoom,
-                                self.image_pos.1 + (point.y - BUTTON_OFFSET) * self.zoom
+                                self.image_pos.0 + (point.x - 4.0) * self.zoom,
+                                self.image_pos.1 + (point.y - 10.0) * self.zoom
                             );
                             ui.set_cursor_pos(draw_point);
                             ui.push_id(i);
                             if ui.radio_button(im_str!(""), &mut self.selected_point, i) {
-                                self.description = point.description.clone();
+                                self.current_description = point.description.clone();
                             }
                             ui.pop_id();
                             i += 1;
@@ -167,7 +191,15 @@ impl RegionWindow {
                     .scrollbar_horizontal(true)
                     .build(|| {
                         let changed = ui.input_text_multiline(im_str!("Input"), &mut desc, ImVec2::new(TEXT_FRAME_WIDTH, TEXT_FRAME_HEIGHT)).build();
-                        if changed {
+                        if changed && !self.readonly {
+                            let desc_str: &str = desc.as_ref();
+                            self.current_description = desc_str.to_owned();
+                            let current_point = self.points.get_mut(self.selected_point as usize);
+                            if current_point.is_some() {
+                                current_point.unwrap().description = self.current_description.clone();
+                            } else {
+                                self.region_description = self.current_description.clone();
+                            }
                         }
                     });
             });
@@ -243,13 +275,16 @@ fn main() {
 
     let mut region_window = RegionWindow {
         name: region_data.name,
-        description: region_data.description,
+        region_description: region_data.description.clone(),
+        current_description: region_data.description,
         image: image,
+        image_name: region_data.image,
         image_size: (size.0 as f32, size.1 as f32),
         image_pos: (0.0, 0.0),
         points: region_data.points,
         selected_point: -1,
         zoom: 0.6,
+        readonly: false,
     };
 
     imgui_glutin_support::configure_keys(&mut imgui);
