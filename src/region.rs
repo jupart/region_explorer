ca4891ae7a4e2eac0bbe6311e4e3dd62dc906bee
jupart <justinwpartain1@gmail.com>
2018-12-18 10:42:59 -0500

    Incremental changes

--- src/region.rs
+++ src/region.rs
@@ -6,19 +6,20 @@ use gtk::prelude::*;
 const TEXT_BORDER_SIZE: i32 = 3;
 const TEXT_PADDING_SIZE: i32 = 3;
 const BUTTON_SIZE: i32 = 20;
-//pub const WINDOW_WIDTH: f32 = 800.0;
-//pub const WINDOW_HEIGHT: f32 = 600.0;
-//const DESCRIPTION_CAPACITY: usize = 5000;
-//const IMAGE_FRAME_WIDTH: f32 = 500.0;
-//const IMAGE_FRAME_HEIGHT: f32 = 540.0;
-//const TEXT_FRAME_WIDTH: f32 = 270.0;
-//const TEXT_FRAME_HEIGHT: f32 = 540.0;
+const WINDOW_WIDTH: i32 = 800;
+const WINDOW_HEIGHT: i32 = 600;
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 struct MapPoint {
     pub x: f32,
     pub y: f32,
     pub description: String,
+}
+
+impl MapPoint {
+    pub fn new(x: f32, y: f32) -> Self {
+        Self { x, y, description: String::new() }
+    }
 }
 
 #[derive(Serialize, Deserialize)]
@@ -111,10 +112,26 @@ impl RegionData {
                 height: BUTTON_SIZE
             })
         });
-        let scrolled_window = gtk::ScrolledWindow::new(None, None);
+        let map = gtk::ScrolledWindow::new(None, None);
         let image = gtk::Image::new_from_file("./resources/kellua saari.png");
-        scrolled_window.add(&image);
-        overlay.add(&scrolled_window);
+        map.add(&image);
+        map.add_events(gdk::EventMask::BUTTON_PRESS_MASK.bits() as i32);
+        map.connect_button_press_event(|map, event| {
+            let right_click = 3;
+            if event.get_button() == right_click {
+                let probably_overlay = map.get_parent().unwrap();
+                let overlay = probably_overlay.downcast_ref::<gtk::Overlay>().unwrap();
+                let probably_scrollbar = overlay.get_child().unwrap();
+                let scrollbar = probably_scrollbar.downcast_ref::<gtk::Scrollbar>().unwrap();
+
+                let (x, y) = event.get_position();
+                let new_point = MapPoint::new(x as f32, y as f32);
+                println!("Creating new point at {:?}", new_point);
+                // overlay.add_overlay(&self.get_map_marker(&new_point));
+            }
+            Inhibit(false)
+        });
+        overlay.add(&map);
 
         for point in &self.points {
             let marker = self.get_map_marker(&point);
@@ -129,7 +146,7 @@ impl RegionData {
         window.set_title("float");
         window.set_border_width(5);
         window.set_position(gtk::WindowPosition::Center);
-        window.set_default_size(800, 600);
+        window.set_default_size(WINDOW_WIDTH, WINDOW_HEIGHT);
         window.connect_delete_event(move |win, _| {
             win.destroy();
             Inhibit(false)
@@ -149,19 +166,7 @@ impl RegionData {
         window.add(&main_box);
         window.show_all();
     }
-
-//    fn mouse_in_region(&self, ui: &Ui) -> bool {
-//        let current_mouse_pos = ui.imgui().mouse_pos();
-//        (current_mouse_pos.0 < IMAGE_FRAME_WIDTH)
-//            && (current_mouse_pos.0 > 10.0)
-//            && (current_mouse_pos.1 > 40.0)
-//            && (current_mouse_pos.1 < IMAGE_FRAME_HEIGHT + 40.0)
-//    }
-
-//    fn mouse_in_frame_coords(&self, ui: &Ui) -> (f32, f32) {
-//        let current_mouse_pos = ui.imgui().mouse_pos();
-//        (current_mouse_pos.0 - 17.0, current_mouse_pos.1 - 54.0)
-//    }
+}
 
 //    fn create_backup(&self) {
 //        std::fs::copy("./resources/kellua_saari.ron", "./resources/kellua_saari.backup").unwrap();
@@ -181,160 +186,3 @@ impl RegionData {
 //        let ron_string = ron::ser::to_string_pretty(&region, pretty).unwrap();
 //        std::fs::write("./resources/kellua_saari.ron", ron_string).unwrap();
 //    }
-
-//    fn remove_point(&mut self, point_to_delete: Option<i32>) {
-//        if point_to_delete.is_none() {
-//            return;
-//        }
-//        let point = point_to_delete.unwrap();
-
-//        // Shift self.selected_point as needed, depending on position of point
-//        if point == self.selected_point {
-//            self.selected_point = -1;
-//            self.current_description = self.region_description.clone();
-//        } else if point < self.selected_point {
-//            self.selected_point -= 1;
-//        }
-//        self.points.remove(point as usize);
-//    }
-
-//    fn handle_input(&mut self, ui: &Ui) {
-//        if ui.imgui().is_key_pressed(VirtualKeyCode::Escape as usize) {
-//            self.to_delete = false;
-//        }
-
-//        // TODO - zoom is working, but there are issues
-//        //      1. I'd like to zoom to mouse pos on scroll
-//        //      2. Radio buttons are not drawn in the right position at all zoom levels
-//        //
-//        // let scroll = ui.imgui().mouse_wheel();
-//        // if scroll != 0.0 {
-//        //     if self.mouse_in_region(&ui) {
-//        //         if scroll > 0.0 {
-//        //             self.zoom += 0.1;
-//        //         } else {
-//        //             self.zoom -= 0.1;
-//        //         }
-
-//        //         if self.zoom > 1.0 {
-//        //             self.zoom = 1.0;
-//        //         }
-//        //         if self.zoom < 0.1 {
-//        //             self.zoom = 0.1;
-//        //         }
-//        //         self.image_pos = ((-self.image_size.0 * self.zoom + IMAGE_FRAME_WIDTH) / 2.0, (-self.image_size.1 * self.zoom + IMAGE_FRAME_HEIGHT) / 2.0);
-//        //     }
-//        // }
-//        if ui.imgui().is_mouse_clicked(ImMouseButton::Right) {
-//            if self.mouse_in_region(&ui) && !self.readonly {
-//                let frame_click = self.mouse_in_frame_coords(&ui);
-//                let point_x = frame_click.0 - self.image_pos.0;
-//                let scaled_point_x = point_x / self.zoom;
-//                let point_y = frame_click.1 - self.image_pos.1;
-//                let scaled_point_y = point_y / self.zoom;
-//                self.points.push(MapPoint::new(scaled_point_x, scaled_point_y, "New shit".to_owned()));
-//            }
-//        }
-//        if ui.imgui().is_mouse_dragging(ImMouseButton::Middle) {
-//            let delta = ui.imgui().mouse_delta();
-//            self.image_pos.0 += delta.0;
-//            self.image_pos.1 += delta.1;
-//        }
-//    }
-
-//    pub fn do_ui(&mut self, ui: &Ui) {
-//        self.handle_input(ui);
-
-//        let mut desc = ImString::with_capacity(DESCRIPTION_CAPACITY);
-//        desc.push_str(self.current_description.as_str());
-
-//        ui.window(im_str!(""))
-//            .position((0.0, 0.0), ImGuiCond::Once)
-//            .scroll_bar(false)
-//            .resizable(false)
-//            .scrollable(false)
-//            .size((WINDOW_WIDTH, WINDOW_HEIGHT), ImGuiCond::Once)
-//            .build(|| {
-//                // Headers
-//                ui.text(self.name.as_str());
-
-//                // For deleting points
-//                ui.same_line(IMAGE_FRAME_WIDTH - 100.0);
-//                ui.checkbox(im_str!("Delete mode"), &mut self.to_delete);
-
-//                // Is readonly?
-//                ui.same_line(IMAGE_FRAME_WIDTH + 20.0);
-//                ui.checkbox(im_str!("Read-only"), &mut self.readonly);
-
-//                if self.to_delete {
-//                    ui.imgui().set_mouse_cursor(ImGuiMouseCursor::Hand);
-//                } else {
-//                    ui.imgui().set_mouse_cursor(ImGuiMouseCursor::Arrow);
-//                }
-//                if self.readonly {
-//                    self.to_delete = false;
-//                }
-
-//                // Write changed descriptions and new points to file
-//                ui.same_line(IMAGE_FRAME_WIDTH + 244.0);
-//                if ui.button(im_str!("Write"), ImVec2::new(45.0, 18.0)) && !self.readonly {
-//                    self.write_file();
-//                }
-
-//                // Map
-//                let mut point_to_delete: Option<i32> = None;
-//                ui.child_frame(im_str!("Map"), (IMAGE_FRAME_WIDTH, IMAGE_FRAME_HEIGHT))
-//                    .movable(false)
-//                    .show_scrollbar_with_mouse(false)
-//                    .show_scrollbar(false)
-//                    .scrollbar_horizontal(false)
-//                    .build(|| {
-//                        ui.set_cursor_pos(self.image_pos);
-//                        ui.image(self.image, ImVec2::new(self.image_size.0 * self.zoom, self.image_size.1 * self.zoom)).build();
-
-//                        let mut i = 0;
-//                        for point in &self.points {
-//                            let draw_point = (
-//                                self.image_pos.0 + (point.x - 4.0) * self.zoom,
-//                                self.image_pos.1 + (point.y - 10.0) * self.zoom
-//                            );
-//                            ui.set_cursor_pos(draw_point);
-//                            ui.push_id(i);
-
-//                            let point_before_select = self.selected_point;
-//                            if ui.radio_button(im_str!(""), &mut self.selected_point, i) {
-//                                if !self.to_delete {
-//                                    self.current_description = point.description.clone();
-//                                } else {
-//                                    self.selected_point = point_before_select;
-//                                    point_to_delete = Some(i);
-//                                }
-//                            }
-//                            ui.pop_id();
-//                            i += 1;
-//                        }
-//                    });
-
-//                self.remove_point(point_to_delete);
-
-//                // Description
-//                ui.same_line(IMAGE_FRAME_WIDTH + 20.0);
-//                ui.child_frame(im_str!("Description"), (TEXT_FRAME_WIDTH, TEXT_FRAME_HEIGHT))
-//                    .scrollbar_horizontal(true)
-//                    .build(|| {
-//                        let changed = ui.input_text_multiline(im_str!("Input"), &mut desc, ImVec2::new(TEXT_FRAME_WIDTH, TEXT_FRAME_HEIGHT)).build();
-//                        if changed && !self.readonly {
-//                            let desc_str: &str = desc.as_ref();
-//                            self.current_description = desc_str.to_owned();
-//                            let current_point = self.points.get_mut(self.selected_point as usize);
-//                            if current_point.is_some() {
-//                                current_point.unwrap().description = self.current_description.clone();
-//                            } else {
-//                                self.region_description = self.current_description.clone();
-//                            }
-//                        }
-//                    });
-//            });
-//    }
-}
-
